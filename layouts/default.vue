61024cbf3640fe864631b12df456bf2ae8d26238
AntonGolova <antongolova@gmail.com>
2024-02-20 01:00:47 +0530

    george24: base structure

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="layout">
+
+    <slot />
+  </div>
+</template>
+
+<style lang="scss">
+html, body {
+  padding: 0;
+  margin: 0;
+}
+
+.layout {
+  background: #F0ECE3;
+  position: relative;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <section class="poster section">
+      <div class="header">header1</div>
+    </section>
+
+    <section class="intro section">intro</section>
+    <section class="works section">works</section>
+    <section class="achieves section">achieves</section>
+    <section class="reviews section">reviews</section>
+    <section class="stack section">stack</section>
+    <section class="call section">call</section>
+    <section class="contacts section">contacts</section>
+  </div>
+</template>
+
+<style lang="scss">
+.header {
+  position: fixed;
+  z-index: 1;
+}
+.section {
+  min-height: 320px;
+  background: #F0ECE3;
+  z-index: 2;
+  position: relative;
+}
+.poster {
+  background: #0F2E2F;
+  min-height: 520px;
+}
+
+
+</style>
