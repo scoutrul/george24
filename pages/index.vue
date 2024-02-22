2fee9e1b29127f900a45ea0e93aaca1473ecee21
AntonGolova <antongolova@gmail.com>
2024-02-22 15:37:57 +0530

    george24: logotype image progress

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
-    <section class="poster section">
-      <div class="header">header1</div>
-    </section>
+  <div class="page">
+    <section class="poster section"></section>
 
-    <section class="intro section">intro</section>
+    <section class="intro section">
+      <logo is-black class="logo-black" />
+    </section>
     <section class="works section">works</section>
     <section class="achieves section">achieves</section>
     <section class="reviews section">reviews</section>
@@ -14,16 +14,14 @@
   </div>
 </template>
 
+<script>
+import Logo from "../components/atoms/logo.vue";
+export default {
+  components: { Logo },
+};
+</script>
 <style lang="scss">
-.header {
-  position: fixed;
-  z-index: 1;
-}
-.section {
-  min-height: 320px;
-  background: #f0ece3;
-  z-index: 2;
-  position: relative;
+@mixin page-padding {
   padding: 16px;
 
   @media (min-width: 900px) {
@@ -38,8 +36,58 @@
     padding: 32px 80px;
   }
 }
+
+.page {
+  position: relative;
+}
+
+.logo-black {
+  position: fixed;
+  z-index: 5;
+  top: 0;
+  left: 10px;
+  @include page-padding;
+}
+
+.logo-white {
+  position: fixed;
+  z-index: 5;
+  top: 0;
+  @include page-padding;
+}
+
+.section {
+  min-height: 320px;
+  background: #f0ece3;
+  z-index: 1;
+  position: relative;
+  @include page-padding;
+}
 .poster {
-  background: #0f2e2f;
+  background-color: #0f2e2f;
   min-height: 520px;
+  z-index: 4;
+
+  background-image: url(@/assets/logo-white.svg);
+  background-size: 64px;
+  background-repeat: no-repeat;
+  background-attachment: fixed;
+  background-position: 27px 16px;
+
+  @media (min-width: 900px) {
+    background-position: 52px 32px;
+  }
+
+  @media (min-width: 1440px) {
+    background-position: 75px 32px;
+  }
+
+  @media (min-width: 1920px) {
+    background-position: 90px 32px;
+  }
+}
+
+.intro {
+  z-index: 3;
 }
 </style>
