dc33639c275fa29bcc892ef1ab2cacb6d405d84a
AntonGolova <antongolova@gmail.com>
2024-03-23 17:21:01 +0530

    Updates

--- assets/default.DLXDRqKT.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,v as s,L as o,t as a}from"./entry.CSa6dui8.js";const c={},r={class:"layout"};function n(e,_){return a(),s("div",r,[o(e.$slots,"default")])}const d=t(c,[["render",n]]);export{d as default};
