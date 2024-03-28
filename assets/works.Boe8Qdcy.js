631f15f23413dbcd1978f7118ba9588b5043e522
AntonGolova <antongolova@gmail.com>
2024-03-28 16:02:50 +0530

    Updates

---
+++ assets/works.Boe8Qdcy.js
@@ -0,0 +1,1 @@
+import{S as o,I as e,W as s}from"./works.DExz0ZEN.js";import{c as a,b as t,w as r,o as c}from"./entry.Bf5-WsSs.js";import"./interval.BieptywK.js";import"./nuxt-link.m46TekiY.js";const n={class:"page"},f={__name:"works",setup(i){return(_,p)=>(c(),a("div",n,[t(e),t(o,null,{default:r(()=>[t(s)]),_:1})]))}};export{f as default};
--- assets/works.CTzSjQII.js
+++
@@ -1,1 +0,0 @@
-import{S as o,I as e,W as s}from"./works.BQmL3bzF.js";import{c as a,b as t,w as r,o as c}from"./entry.ERPEOSYA.js";import"./interval.CHtOi11K.js";import"./nuxt-link.BxJay8XO.js";const n={class:"page"},f={__name:"works",setup(i){return(_,p)=>(c(),a("div",n,[t(e),t(o,null,{default:r(()=>[t(s)]),_:1})]))}};export{f as default};
