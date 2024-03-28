4152b4de3d017e14fd1062ea551b954e28b6580b
AntonGolova <antongolova@gmail.com>
2024-03-28 15:51:38 +0530

    Updates

--- george24/assets/works.R5-oVWXl.js
+++
@@ -1,1 +0,0 @@
-import{S as o,I as e,W as s}from"./works.CNT7BgNX.js";import{c as a,b as t,w as r,o as c}from"./entry.DlCCciC0.js";import"./interval.DG51QFc5.js";import"./nuxt-link.D1zPkbil.js";const n={class:"page"},f={__name:"works",setup(i){return(_,p)=>(c(),a("div",n,[t(e),t(o,null,{default:r(()=>[t(s)]),_:1})]))}};export{f as default};
