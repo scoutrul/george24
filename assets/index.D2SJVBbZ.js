557f502c0eee63bbbe04fc250d03075016f4cb34
AntonGolova <antongolova@gmail.com>
2024-03-29 15:34:59 +0530

    Updates

---
+++ assets/index.D2SJVBbZ.js
@@ -0,0 +1,1 @@
+import{S as o,I as e,W as s}from"./works.C-r4qtmT.js";import{c as r,b as t,w as a,o as c}from"./entry.sHarKHGP.js";import"./interval.CH9AegR9.js";import"./nuxt-link.Dtd3u5iS.js";const n={class:"works"},f={__name:"index",setup(i){return(_,m)=>(c(),r("div",n,[t(e),t(o,null,{default:a(()=>[t(s)]),_:1})]))}};export{f as default};
